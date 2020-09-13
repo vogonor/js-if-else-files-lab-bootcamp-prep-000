12147ef4b3ddbd2b20990016fbde4387797bd5ee
observant-algorithm-6319 <vogonor@yahoo.com>
2020-09-13 21:57:35 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
-var name = "Joe"
-var height = "74"
+var name = "Joe";
+  if(name === "Susan"){
+    console.log("The name is correct");
+  } else {
+    console.log("Expected the name to be Susan");
+  }
+var height = "74";
 
 // Don't worry about this
-module.exports = { name, height
-}
-
+ module.exports = { name, height
+ }
